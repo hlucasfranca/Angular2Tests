cb8e76b675f32a38069771b38371aa90530699f5
Henrique <hlucasfranca@gmail.com>
2017-01-25 10:41:56 -0200

    Testes flexbox

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+    title = 'Formulário de Busca';
+
+    data = [];
+
+    constructor() {
+        for (let i = 0, c = 'a'; i < 10; i++ , c += c) {
+            this.data.push({
+                code: i,
+                name: c
+            });
+        }
+    }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { MaterialModule } from '@angular/material';
+import {FlexLayoutModule} from '@angular/flex-layout';
+
+
+import 'hammerjs';
 
 import { AppComponent } from './app.component';
 
@@ -12,7 +17,9 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    MaterialModule.forRoot(),
+    FlexLayoutModule.forRoot(),
   ],
   providers: [],
   bootstrap: [AppComponent]
